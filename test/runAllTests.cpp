9b841a0c974b284db23aa8632476af5fb1851b46
RaduRu <radu.ursu@stud.unifi.it>
2025-03-28 17:26:54 +0100

    unit testing creata, creazione runAllTests.cpp, sistemato CMakeLists.txt nella directory test

---
+++ test/runAllTests.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Radu Ursu on 28/03/25.
+//
+
+
+#include "gtest/gtest.h"
+
+int main(int argc, char ** argv){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
